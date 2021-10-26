5f422aa849a3fd5e861c9fc1b96e428543c59ce8
Caleb Smith <caleb@myrvmail.com>
2021-10-26 10:21:01 -0500

    Rewrite He he, that didn't take long

--- README.md
+++ README.md
@@ -1,22 +1,35 @@
-# twitter-bot-rs
-A Twitter bot written in Rust that scans crypto-currency values and
-post periodically a message on Twitter with the value of currencies.
+# Rusted Parrot
 
-![example of tweet](misc/tweet.png)
+A simple Twitter bot written in Rust that tweets from a text file one line at a
+time at regular intervals. The main purpose is for quote bots, but it can really
+be used for anything with a static list of tweets.
 
+## Usage 
 
-## How to run it
-1. Edit ```$HOME/.crypto-bot.conf``` and put values your want in your config file
-2. Issue ```cargo run```
+1. Create a config folder
+   - `~/.config/rusted-parrot` on Linux
+   - `%APPDATA%\rusted-parrot` on Windows
+2. Create the config file using the template below, and save it as
+   `config.json` in the config folder you created.
+3. Create your tweet text file, one tweet/quote per line, and save it as 
+   `quotes.txt` in the config folder.
+4. Run `rusted-parrot`, (or `cargo run` if building from source)
 
 ## Config File
+
 ```
 {
   "consumer_key": "<consumer-key>",
   "consumer_secret": "<consumer-secret>",
   "access_key": "<access-key>",
   "access_secret": "<access-secret>",
-  "currencies_to_follow" : ["BTC","LTC","ETH", "DASH", "ZEC", "XRP"],
-  "interval_sec" : 5
+  "interval_hours" : <num of hours between tweets>
 }
 ```
+
+## License
+
+This project is licensed under the GPLv3 or later. It was originally based on
+`twitter-bot-rs` at [https://github.com/juli1/twitter-bot-rs], but that project
+seems to be abandoned with no license and in any case very little of the original
+code remains in this project.
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,48 @@
-use std::path::PathBuf;
-use std::time;
-
-mod config;
-mod twitter;
+use serde::{Deserialize, Serialize};
 
 const CONF_FILENAME: &str = "config.json";
 const QUOTE_FILENAME: &str = "quotes.txt";
 
-#[tokio::main]
-async fn main() {
-    let conf_path: PathBuf = dirs2::config_dir().expect("Failed to get config dir!").join("twitter-bot");
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Config {
+    pub consumer_key: String,
+    pub consumer_secret: String,
+    pub access_key: String,
+    pub access_secret: String,
+    pub interval_hours: u64,
+}
 
-    let config =
-        config::Config::read(&conf_path.join(CONF_FILENAME)).expect("Cannot find config file");
+fn main() {
+    let conf_path: std::path::PathBuf = dirs2::config_dir()
+        .expect("Failed to get config dir!")
+        .join("rusted-parrot");
+
+    let config: Config = serde_json::from_str(
+        &std::fs::read_to_string(&conf_path.join(CONF_FILENAME))
+            .expect("Could not open config file"),
+    )
+    .expect("Could not parse config file");
 
     let quotes: Vec<String> = std::fs::read_to_string(&conf_path.join(QUOTE_FILENAME))
         .expect("Cannot find quote source file")
         .lines()
         .map(|s| s.to_owned())
         .collect();
-    let twitter = twitter::Twitter::new(
-        config.consumer_key,
-        config.consumer_secret,
-        config.access_key,
-        config.access_secret,
-    );
+
+    let twitter_bot = rust_twitter_bot_lib::TwitterBot::new()
+        .consumer_key(&config.consumer_key)
+        .consumer_secret_key(&config.consumer_secret)
+        .access_token(&config.access_key)
+        .secret_access_token(&config.access_secret);
 
     for q in quotes {
-        futures::executor::block_on(twitter.tweet(q));
+        match twitter_bot.tweet(&q, None) {
+            Ok(_) => println!("Tweeted \"{}\"", q),
+            Err(e) => panic!("{:?}", e),
+        };
 
-        std::thread::sleep(time::Duration::from_millis(1000 * 60 * 60 * config.interval_hours));
+        std::thread::sleep(std::time::Duration::from_millis(
+            1000 * 60 * 60 * config.interval_hours,
+        ));
     }
 }
